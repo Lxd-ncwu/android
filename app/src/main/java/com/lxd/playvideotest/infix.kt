3589cdc1da9cf0652e8f5ee32a7e14ea0a6d27eb
Lxd-ncwu <1750402901@qq.com>
2022-06-28 20:59:24 +0800

    First commit

---
+++ app/src/main/java/com/lxd/playvideotest/infix.kt
@@ -0,0 +1,9 @@
+infix fun String.beginWith(prefix: String) = startsWith(prefix)
+
+infix fun <T> Collection<T>.has(element: T) = contains(element)
+
+fun main() {
+    if ("Hello Kotlin" beginWith "Hello") {
+        println(12)
+    }
+}
